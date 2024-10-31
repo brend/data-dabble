22da9d40ac65554cb7b5fd379103082ef3151f9d
Philipp Brendel <philipp.brendel@ianeo.de>
2024-10-31 13:07:36 +0100

    rework explorer

--- src-tauri/src/explorer.rs
+++ src-tauri/src/explorer.rs
@@ -18,39 +18,50 @@ pub struct ExplorerNode {
     pub leaf: bool,
 }
 
+impl ExplorerNode {
+    pub fn new(key: &str, label: &str, node_type: NodeType, leaf: bool) -> Self {
+        Self {
+            key: key.to_string(),
+            label: label.to_string(),
+            node_type,
+            leaf,
+        }
+    }
+}
+
 pub fn get_data_source_nodes(parent_node_key: Option<String>) -> Vec<ExplorerNode> {
     if let Some(parent_node_key) = parent_node_key {
         match parent_node_key.as_str() {
             "1" => vec![
-                ExplorerNode {
-                    key: "1.1".to_string(),
-                    label: "Gigantos - Sales".to_string(),
-                    node_type: NodeType::DataSource,
-                    leaf: false,
-                },
-                ExplorerNode {
-                    key: "1.2".to_string(),
-                    label: "Gigantos - HR".to_string(),
-                    node_type: NodeType::DataSource,
-                    leaf: false,
-                },
+                ExplorerNode.new(
+                    "1.1".to_string(),
+                    "Gigantos - Sales".to_string(),
+                    NodeType::DataSource,
+                    false,
+                ),
+                ExplorerNode.new(
+                    "1.2".to_string(),
+                    "Gigantos - HR".to_string(),
+                    NodeType::DataSource,
+                    false,
+                ),
             ],
             _ => vec![],
         }
     } else {
         vec![
-            ExplorerNode {
-                key: "1".to_string(),
-                label: "Gigantos (MS SQL)".to_string(),
-                node_type: NodeType::DataSource,
-                leaf: false,
-            },
-            ExplorerNode {
-                key: "2".to_string(),
-                label: "Persephone (Oracle DB)".to_string(),
-                node_type: NodeType::DataSource,
-                leaf: false,
-            },
+            ExplorerNode.new(
+                "1".to_string(),
+                "Gigantos (MS SQL)".to_string(),
+                NodeType::DataSource,
+                false,
+            ),
+            ExplorerNode.new(
+                "2".to_string(),
+                "Persephone (Oracle DB)".to_string(),
+                NodeType::DataSource,
+                false,
+            ),
         ]
     }
 }
